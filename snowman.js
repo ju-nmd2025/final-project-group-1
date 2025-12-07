d5262381e71b2ff7faeb00f67e1164c562be85f4
sjel25ng <sjel25ng@student.ju.se>
2025-12-07 20:03:36 +0100

    Update snowman.js

--- snowman.js
+++ snowman.js
@@ -1,17 +1,48 @@
-function setup() {
-    createCanvas(500, 700);
-    noStroke();
-}
+// Snowman
 
-function draw() {
-    background(220);
-    drawSnowman(250, 500);
+export default class Snowman {
+    constructor(x, y, w, h, fallspeed, jumpDistance) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.fallspeed = fallspeed;
+        this.jumpDistance = jumpDistance;
+    }
+
+    fall() {
+        this.y += this.fallspeed;
+    }
+    
+    jump() {
+        this.y -= this.jumpDistance;
+    }
+
+    isColliding(objects, gameSpeed) {
+        for (const object of objects) {
+            if (
+                Math.abs(this.y + this.h - object.y) < gameSpeed &&
+                this.x + this.w >= object.x &&
+                this.x <= object.x + object.w
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    draw() {
+        push();
+        fill("white");
+        drawSnowman(this.x, this.y);
+        pop();
+    }
 }
 
 function drawSnowman(x, y) {
-    // Leg cirkle
+    // Leg circle (fixed!)
     fill(255);
-    ellipse(x, y, - 50, 50);
+    ellipse(x, y, 50, 50);
     
     // Middle circle
     ellipse(x, y - 40, 40, 40);
@@ -26,6 +57,10 @@ function drawSnowman(x, y) {
     
     // Carrot nose
     fill(255, 165, 0);
-    triangle(x, y - 67, x + 12, y -65, x, y - 63);
-}
+    triangle(x, y - 67, x + 12, y - 65, x, y - 63);
 
+    // Buttons
+    fill(0);
+    ellipse(x, y - 50, 5, 5);
+    ellipse(x, y - 35, 5, 5);
+}
