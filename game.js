e5a48ef081fd5659a61e4fe5f493c98b548f516e
saaaraaaaa15 <sarak05@icloud.com>
2025-12-08 16:35:11 +0100

    created platform removed old code

--- game.js
+++ game.js
@@ -1,42 +1,56 @@
-import { character } from "./character";
-import platform from "platform";
+//import  character  from "./character.js";
+import Platform from "./platform.js";
+
+// //let character;
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(500, 700);
+
+  //Create the player
+  //character = new Character();
+
+  //Create test platform
 }
 
-// Obstacle / Spike / Death
-function drawObstacle() {
-    push();
-    fill("red");
-    triangle(180, 300, 210, 240, 240, 300);
-    pop();
-}
-
-let x = 100;
-let y = 100;
+// //let x = 100;
+// //let y = 100;
+let platform = new Platform(200, 200, "normal");
 
 function draw() {
-    background(100, 100, 100);
+  background(100, 100, 100);
 
-    character.draw();
-	platform.draw();
-
-    platform.x -= 10;
-    if(platform.x + platform.w < 0){
-        platform.x = 500;
-    }
-
-    if(character.y + character.h < 300){
-        character.y += 10;
-    }
-
-    // Floor
-    line(0, 300, 400, 300);
+  //Draw platform
+  platform.update();
+  platform.draw();
 }
 
-function keyPressed(){
-    if(character.y + character.h === 300){
-        character.y -= 80;
-    }
-}
+// //Move platform left to right
+// //platform.x -= 2;
+// //if (platform.x + platform.w < 0) {
+// //platform.x = width;
+// //}
+
+// //character.draw();
+
+// //createPlatform(200);
+
+// //platform.draw();
+
+// //platform.x -= 10;
+// //if(platform.x + platform.w < 0){
+// //platform.x = 500;
+// //}
+
+// //if(character.y + character.h < 300){
+// //character.y += 10;
+// //}
+
+// // Floor
+// //line(0, 300, 400, 300);
+// //}
+
+// //function keyPressed(){
+// //if(character.y + character.h === 300){
+// //character.y -= 80;
+// //}
+// //}
--- platform.js
+++ platform.js
@@ -1,13 +1,57 @@
-export let platform = {
-    x: 250,
-    y: 250,
-    w: 80,
-    h: 20,
+// platform 
+export default class Platform {
+    constructor(x, y, type = "normal") {
+        this.x = x;
+        this.y = y;
+        this.w = 70;
+        this.h = 18;
+        this.type = type;
+
+        this.speed = type === "moving" ? random(1, 2) * (random() < 0.5 ? -1 : 1) : 0;
+    }
+
+    update () {
+        if (this.type === "moving") {
+            this.x += this.speed;
+            if (this.x < 0 || this.x > width - this.w) {
+                this.speed *= -1;
+            }
+        }
+    }
+
+    drawCandyCane() {
+        // Background White
+        fill(255);
+        rect(this.x, this.y, this.w, this.h);
+
+        //Candy Cane Pattern
+        stroke(255, 0, 0);
+        strokeWeight(4);
+
+        for (let i = -20; i < this.w + 20; i+= 12) {
+            line(this.x + i, this.y, this.x + i + 20, this.y + this.h);
+        }
+
+        strokeWeight(1);
+        noStroke();
+    }
 
     draw() {
         push();
-        fill("blue");
+        
+        //Color Tint Based on Type
+        if (this.type === "normal") fill(255);
+        if (this.type === "moving") fill (220, 240, 255);
+        if (this.type === "breaking") fill(255, 200, 200);
+
+        //Draw Candy Cane Pattern
+        this.drawCandyCane();
+
+        //Outline
+        noFill();
+        stroke(150);
         rect(this.x, this.y, this.w, this.h);
+
         pop();
-    },
-};
+    }
+}                             
