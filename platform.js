8abf7477ed48403e822afee4f4a125672403e513
saaaraaaaa15 <sarak05@icloud.com>
2025-12-08 20:38:39 +0100

    ny platform#

--- platform.js
+++ platform.js
@@ -1,57 +1,43 @@
-// platform 
+// platform
 export default class Platform {
-    constructor(x, y, type = "normal") {
-        this.x = x;
-        this.y = y;
-        this.w = 70;
-        this.h = 18;
-        this.type = type;
+  constructor(x, y, type = "normal") {
+    this.x = x;
+    this.y = y;
+    this.w = 60; //bredd
+    this.h = 10; //höjd
+    this.type = type; //typ av platform
 
-        this.speed = type === "moving" ? random(1, 2) * (random() < 0.5 ? -1 : 1) : 0;
+    this.speed = type === "moving" ? 1.5 : 0; //hastighet, 1.5, annars 0
+    this.direction = 1; //1=höger, -1=vänster
+    this.broken = false; //sant när en plattform går sönder
+  }
+
+  update() {
+    //moving platforms ändrar x position
+    if (this.type === "moving") {
+      this.x += this.speed * this.direction;
+
+      if (this.x <= 0 || this.x >= width - this.w) {
+        //byter håll när den når kanten
+        this.direction *= -1;
+      }
     }
+  }
 
-    update () {
-        if (this.type === "moving") {
-            this.x += this.speed;
-            if (this.x < 0 || this.x > width - this.w) {
-                this.speed *= -1;
-            }
-        }
+  show() {
+    if (this.type === "normal") fill(255, 0, 0); //röd
+    else if (this.type === "moving") fill(255, 80, 80); //ljusare röd
+    else if (this.type === "breaking") fill(255, 130, 130); //röd/rosa
+
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  onJump() {
+    //när snögubben hoppar på platformen
+    if (this.type === "breaking") {
+      //broken platform
+      this.broken = true;
+      this.y = height + 200;
     }
-
-    drawCandyCane() {
-        // Background White
-        fill(255);
-        rect(this.x, this.y, this.w, this.h);
-
-        //Candy Cane Pattern
-        stroke(255, 0, 0);
-        strokeWeight(4);
-
-        for (let i = -20; i < this.w + 20; i+= 12) {
-            line(this.x + i, this.y, this.x + i + 20, this.y + this.h);
-        }
-
-        strokeWeight(1);
-        noStroke();
-    }
-
-    draw() {
-        push();
-        
-        //Color Tint Based on Type
-        if (this.type === "normal") fill(255);
-        if (this.type === "moving") fill (220, 240, 255);
-        if (this.type === "breaking") fill(255, 200, 200);
-
-        //Draw Candy Cane Pattern
-        this.drawCandyCane();
-
-        //Outline
-        noFill();
-        stroke(150);
-        rect(this.x, this.y, this.w, this.h);
-
-        pop();
-    }
-}                             
+  }
+}
