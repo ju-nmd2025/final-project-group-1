e882dda2289d4976392df4ac3daa98c73f6664d8
sjel25ng <sjel25ng@student.ju.se>
2025-12-09 15:17:02 +0100

    Update character.js

--- character.js
+++ character.js
@@ -3,7 +3,33 @@ export let character = {
     y: 50,
     w: 50,
     h: 50,
-    draw() {
-        rect(this.x, this.y, this.w, this.h);
-    },
-};
+    
+    draw(x, y) {
+        this.x = x;
+        this.y = y;
+
+        //Snowman 
+        fill(255);
+        noStroke();
+
+        // Bottom circle
+        fill(255);
+        ellipse(x, y, 50, 50);
+        // Middle circle
+        ellipse(x, y - 40, 40, 40);
+        // Head
+        ellipse(x, y - 70, 30, 30);
+        // Eyes
+        fill(0);
+        ellipse(x - 5, y - 75, 5, 5);
+        ellipse(x + 5, y - 75, 5, 5);
+        // Carrot nose
+        fill(255, 165, 0);
+        triangle(x, y - 67, x + 12, y - 65, x, y - 63);
+        // Button
+        fill(0);
+        ellipse(x, y - 50, 5, 5);
+        ellipse(x, y - 35, 5, 5);
+    }
+
+}
